06e19738386f4f290d7b0d280336f8adee7afeb5
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-05-08 20:34:57 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -13,19 +13,26 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdio.h>
 # include <stdlib.h>
+# include <fcntl.h>
 # include <unistd.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
+char	*ft_strchr(const char *s, int c);
+
+char	*ft_exstrchr(char *s);
+
+size_t	ft_strlen(const char *str);
+
+char	*ft_strnldup(char *s1);
+
+char	*ft_strjoin(char *s1, char *s2);
+
+char	*ft_strdup(const char *s1);
 
 char	*get_next_line(int fd);
 
-size_t	ft_strlen(const char *str);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-char	*ft_strjoin(const char *s1, const char *s2);
-char	*ft_strdup(const char *s);
-char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcpy(char *dst, char *src, size_t size);
 
 #endif
+
